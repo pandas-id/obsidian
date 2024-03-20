aff1532701abfe289189c3867516f61b8b757497
Lukhe <akmalthemalong.com>
2024-03-20 16:48:01 +0800

    vault backup: 2024-03-20 16:48:00

---
+++ sync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+current_datetime=$(date +"%Y-%m-%d %T")
+# echo $current_datetime
+commit_message="vault backup: "${current_datetime}
+
+git add *
+git commit -m "$commit_message"
+
+git add .obsidian
+git commit -m "changes (settings, plugins, snippets)"${current_datetime}
+
+git push origin main
